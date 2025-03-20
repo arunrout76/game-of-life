82b5571ed744f4d3f687df1213ad04d96acf4991
arunrout-at-514304094524 <arun.rout@wizklub.com>
2025-03-20 17:07:25 +0530

    Added Print Statement in Home Page

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/HomePage.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/HomePage.java
@@ -16,6 +16,7 @@ public class HomePage extends GameOfLifePage {
     }
 
     public void clickOnNewGameLink() {
+        System.out.println("Clicking on New Game Link");
         newGameLink.click();
     }
 }
